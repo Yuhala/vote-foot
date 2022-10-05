05662883faf8f317f4e3da5978bf6cdf7cc23f69
Yuhala <petersonyuhala@gmail.com>
2022-10-05 19:12:02 +0200

    adding last column to results page

--- resources/views/votes.blade.php
+++ resources/views/votes.blade.php
@@ -2,275 +2,289 @@
 
 
 @section('page-title')
-    <div style="text-align:center;padding-bottom:40px" class="center">
+<div style="text-align:center;padding-bottom:40px" class="center">
 
-        <img src="images/wc_2022.png" alt="WC 2022">
-        <br><br>
-        <h1>Le Vote des Internautes!</h1>
-        <br><br>
-    </div>
+    <img src="images/wc_2022.png" alt="WC 2022">
+    <br><br>
+    <h1>Le Vote des Internautes!</h1>
+    <br><br>
+</div>
 @endsection
 
 @section('voting-results')
-    <div style="text-align:center;padding-bottom:20px">
+<div style="text-align:center;padding-bottom:20px">
 
-        <h2>Gardiens</h2>
+    <h2>Gardiens</h2>
 
-    </div>
-    <table class="table table-striped projects">
-        <thead>
-            <tr>
-                <th style="width: 10%">
+</div>
+<table class="table table-striped projects">
+    <thead>
+        <tr>
+            <th style="width: 10%">
 
-                </th>
-                <th style="width: 20%">
-                    Joeur
-                </th>
-                <th style="width: 25%">
-                    Avatar
-                </th>
-                <th style="width: 40%">
-                    Pourcentage de Votes
-                </th>
+            </th>
+            <th style="width: 20%">
+                Joeur
+            </th>
+            <th style="width: 15%">
+                Avatar
+            </th>
+            <th style="width: 45%">
+                Pourcentage de Votes
+            </th>
+            <th style="width: 10%">
 
-            </tr>
-        </thead>
-        @foreach ($goalies as $goalie)
-            <tbody>
-                <tr>
-                    <td>
+            </th>
 
-                    </td>
-                    <td>
-                        <h3>{{ $goalie->playerName }}</h3>
-                        <br />
-                        <h4>
-                            {{ $goalie->clubName }}
-                        </h4>
-                    </td>
-                    <td>
-                        <ul class="list-inline">
-                            <li class="list-inline-item">
-                                <img alt="Avatar" style="width:75px;height:75px;" class="table-avatar"
-                                    src="{{ $goalie->imagePath }}">
-                            </li>
+        </tr>
+    </thead>
+    @foreach ($goalies as $goalie)
+    <tbody>
+        <tr>
+            <td>
 
-                        </ul>
-                    </td>
-                    <td class="project_progress">
-                        <div class="progress progress-sm">
-                            <div class="progress-bar bg-green" role="progressbar" aria-valuenow="{{ $goalie->votePercent }}"
-                                aria-valuemin="0" aria-valuemax="100" style="width: {{ $goalie->votePercent }}%">
-                            </div>
-                        </div>
-                        <h4>
-                            {{ $goalie->votePercent }}%
-                        </h4>
-                    </td>
+            </td>
+            <td>
+                <h3>{{ $goalie->playerName }}</h3>
+                <br />
+                <h4>
+                    {{ $goalie->clubName }}
+                </h4>
+            </td>
+            <td>
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <img alt="Avatar" style="width:75px;height:75px;" class="table-avatar" src="{{ $goalie->imagePath }}">
+                    </li>
 
-                </tr>
+                </ul>
+            </td>
+            <td class="project_progress">
+                <div class="progress progress-sm">
+                    <div class="progress-bar bg-green" role="progressbar" aria-valuenow="{{ $goalie->votePercent }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $goalie->votePercent }}%">
+                    </div>
+                </div>
+                <h4>
+                    {{ $goalie->votePercent }}%
+                </h4>
+            </td>
+            <td>
 
-            </tbody>
-        @endforeach
-    </table>
+            </td>
 
-    <br>
+        </tr>
 
-    <div style="text-align:center;padding-bottom:20px">
+    </tbody>
+    @endforeach
+</table>
 
-        <h2>Défenseurs</h2>
+<br>
 
-    </div>
-    <table class="table table-striped projects">
-        <thead>
-            <tr>
-                <th style="width: 10%">
+<div style="text-align:center;padding-bottom:20px">
 
-                </th>
-                <th style="width: 20%">
-                    Joeur
-                </th>
-                <th style="width: 25%">
-                    Avatar
-                </th>
-                <th style="width: 40%">
-                    Pourcentage de Votes
-                </th>
+    <h2>Défenseurs</h2>
 
-            </tr>
-        </thead>
-        @foreach ($defenders as $defender)
-            <tbody>
-                <tr>
-                    <td>
+</div>
+<table class="table table-striped projects">
+    <thead>
+        <tr>
+            <th style="width: 10%">
 
-                    </td>
-                    <td>
-                        <h3>{{ $defender->playerName }}</h3>
-                        <br />
-                        <h4>
-                            {{ $defender->clubName }}
-                        </h4>
-                    </td>
-                    <td>
-                        <ul class="list-inline">
-                            <li class="list-inline-item">
-                                <img alt="Avatar" style="width:75px;height:75px;" class="table-avatar"
-                                    src="{{ $defender->imagePath }}">
-                            </li>
+            </th>
+            <th style="width: 20%">
+                Joeur
+            </th>
+            <th style="width: 15%">
+                Avatar
+            </th>
+            <th style="width: 45%">
+                Pourcentage de Votes
+            </th>
+            <th style="width: 10%">
 
-                        </ul>
-                    </td>
-                    <td class="project_progress">
-                        <div class="progress progress-sm">
-                            <div class="progress-bar bg-green" role="progressbar"
-                                aria-valuenow="{{ $defender->votePercent }}" aria-valuemin="0" aria-valuemax="100"
-                                style="width: {{ $defender->votePercent }}%">
-                            </div>
-                        </div>
-                        <h4>
-                            {{ $defender->votePercent }}%
-                        </h4>
-                    </td>
+            </th>
 
-                </tr>
+        </tr>
+    </thead>
+    @foreach ($defenders as $defender)
+    <tbody>
+        <tr>
+            <td>
 
-            </tbody>
-        @endforeach
+            </td>
+            <td>
+                <h3>{{ $defender->playerName }}</h3>
+                <br />
+                <h4>
+                    {{ $defender->clubName }}
+                </h4>
+            </td>
+            <td>
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <img alt="Avatar" style="width:75px;height:75px;" class="table-avatar" src="{{ $defender->imagePath }}">
+                    </li>
 
-    </table>
+                </ul>
+            </td>
+            <td class="project_progress">
+                <div class="progress progress-sm">
+                    <div class="progress-bar bg-green" role="progressbar" aria-valuenow="{{ $defender->votePercent }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $defender->votePercent }}%">
+                    </div>
+                </div>
+                <h4>
+                    {{ $defender->votePercent }}%
+                </h4>
+            </td>
+            <td>
 
-    <br>
-    <div style="text-align:center;padding-bottom:20px">
+            </td>
 
-        <h2>Milieux</h2>
+        </tr>
 
-    </div>
-    <table class="table table-striped projects">
-        <thead>
-            <tr>
-                <th style="width: 10%">
+    </tbody>
+    @endforeach
 
-                </th>
-                <th style="width: 20%">
-                    Joeur
-                </th>
-                <th style="width: 25%">
-                    Avatar
-                </th>
-                <th style="width: 40%">
-                    Pourcentage de Votes
-                </th>
+</table>
 
-            </tr>
-        </thead>
-        @foreach ($midfielders as $mid)
-            <tbody>
-                <tr>
-                    <td>
+<br>
+<div style="text-align:center;padding-bottom:20px">
 
-                    </td>
-                    <td>
-                        <h3>{{ $mid->playerName }}</h3>
-                        <br />
-                        <h4>
-                            {{ $mid->clubName }}
-                        </h4>
-                    </td>
-                    <td>
-                        <ul class="list-inline">
-                            <li class="list-inline-item">
-                                <img alt="Avatar" style="width:75px;height:75px;" class="table-avatar"
-                                    src="{{ $mid->imagePath }}">
-                            </li>
+    <h2>Milieux</h2>
 
-                        </ul>
-                    </td>
-                    <td class="project_progress">
-                        <div class="progress progress-sm">
-                            <div class="progress-bar bg-green" role="progressbar" aria-valuenow="{{ $mid->votePercent }}"
-                                aria-valuemin="0" aria-valuemax="100" style="width: {{ $mid->votePercent }}%">
-                            </div>
-                        </div>
-                        <h4>
-                            {{ $mid->votePercent }}%
-                        </h4>
-                    </td>
+</div>
+<table class="table table-striped projects">
+    <thead>
+        <tr>
+            <th style="width: 10%">
 
-                </tr>
+            </th>
+            <th style="width: 20%">
+                Joeur
+            </th>
+            <th style="width: 15%">
+                Avatar
+            </th>
+            <th style="width: 45%">
+                Pourcentage de Votes
+            </th>
+            <th style="width: 10%">
 
-            </tbody>
-        @endforeach
-    </table>
+            </th>
+
+        </tr>
+    </thead>
+    @foreach ($midfielders as $mid)
+    <tbody>
+        <tr>
+            <td>
+
+            </td>
+            <td>
+                <h3>{{ $mid->playerName }}</h3>
+                <br />
+                <h4>
+                    {{ $mid->clubName }}
+                </h4>
+            </td>
+            <td>
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <img alt="Avatar" style="width:75px;height:75px;" class="table-avatar" src="{{ $mid->imagePath }}">
+                    </li>
+
+                </ul>
+            </td>
+            <td class="project_progress">
+                <div class="progress progress-sm">
+                    <div class="progress-bar bg-green" role="progressbar" aria-valuenow="{{ $mid->votePercent }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $mid->votePercent }}%">
+                    </div>
+                </div>
+                <h4>
+                    {{ $mid->votePercent }}%
+                </h4>
+            </td>
+            <td>
+
+            </td>
+
+        </tr>
+
+    </tbody>
+    @endforeach
+</table>
 
 
 
-    <br>
-    <div style="text-align:center;padding-bottom:20px">
+<br>
+<div style="text-align:center;padding-bottom:20px">
 
-        <h2>Attaquants</h2>
+    <h2>Attaquants</h2>
 
-    </div>
-    <table class="table table-striped projects">
-        <thead>
-            <tr>
-                <th style="width: 10%">
+</div>
+<table class="table table-striped projects">
+    <thead>
+        <tr>
+            <th style="width: 10%">
 
-                </th>
-                <th style="width: 20%">
-                    Joeur
-                </th>
-                <th style="width: 25%">
-                    Avatar
-                </th>
-                <th style="width: 40%">
-                    Pourcentage de Votes
-                </th>
+            </th>
+            <th style="width: 20%">
+                Joeur
+            </th>
+            <th style="width: 15%">
+                Avatar
+            </th>
+            <th style="width: 45%">
+                Pourcentage de Votes
+            </th>
+            <th style="width: 10%">
 
-            </tr>
-        </thead>
-        @foreach ($attackers as $attacker)
-            <tbody>
-                <tr>
-                    <td>
+            </th>
 
-                    </td>
-                    <td>
-                        <h3>{{ $attacker->playerName }}</h3>
-                        <br />
-                        <h4>
-                            {{ $attacker->clubName }}
-                        </h4>
-                    </td>
-                    <td>
-                        <ul class="list-inline">
-                            <li class="list-inline-item">
-                                <img alt="Avatar" style="width:75px;height:75px;" class="table-avatar"
-                                    src="{{ $attacker->imagePath }}">
-                            </li>
+        </tr>
+    </thead>
+    @foreach ($attackers as $attacker)
+    <tbody>
+        <tr>
+            <td>
 
-                        </ul>
-                    </td>
-                    <td class="project_progress">
-                        <div class="progress progress-sm">
-                            <div class="progress-bar bg-green" role="progressbar"
-                                aria-valuenow="{{ $attacker->votePercent }}" aria-valuemin="0" aria-valuemax="100"
-                                style="width: {{ $attacker->votePercent }}%">
-                            </div>
-                        </div>
-                        <h4>
-                            {{ $attacker->votePercent }}%
-                        </h4>
-                    </td>
+            </td>
+            <td>
+                <h3>{{ $attacker->playerName }}</h3>
+                <br />
+                <h4>
+                    {{ $attacker->clubName }}
+                </h4>
+            </td>
+            <td>
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <img alt="Avatar" style="width:75px;height:75px;" class="table-avatar" src="{{ $attacker->imagePath }}">
+                    </li>
 
-                </tr>
+                </ul>
+            </td>
+            <td class="project_progress">
+                <div class="progress progress-sm">
+                    <div class="progress-bar bg-green" role="progressbar" aria-valuenow="{{ $attacker->votePercent }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $attacker->votePercent }}%">
+                    </div>
+                </div>
+                <h4>
+                    {{ $attacker->votePercent }}%
+                </h4>
+            </td>
+            <td>
 
-            </tbody>
-        @endforeach
-    </table>
+            </td>
 
-    <br><br>
-    <div style="text-align:center;" class="center">
-        <span>{{ $totalVotes }} votes.</span>
-    </div>
+        </tr>
+
+    </tbody>
+    @endforeach
+</table>
+
+<br><br>
+<div style="text-align:center;" class="center">
+    <span>{{ $totalVotes }} votes.</span>
+</div>
 @endsection
